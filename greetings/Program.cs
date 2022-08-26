12244a4b7a7ba66f5a98aae7c3ad80f1f4b38441
Mike <mike@MacBook-Air-Mike.local>
2022-08-26 15:10:55 +0300

    Add greetings but something didnt work

---
+++ greetings/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Enter usermane ");
+Console.ReadLine();
+string username = Console.ReadLine();
+
+if (usermane ==  "Mike");
+{
+    Console.WriteLine("WHATSUP NIGGA! ");
+}
+else 
+{
+    Console.Write("Hello, ");
+    Console.Write(usermane);
+}
