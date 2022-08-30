a7fc3a42d75cc905e7f8057033f00d2f5cee0237
«Mike <fox9403@gmail.com>
2022-08-30 16:42:56 +0300

    add full code

--- ex007_array/Program.cs
+++ ex007_array/Program.cs
@@ -1,6 +1,6 @@
 ﻿void FillArray(int[] collection)
 {
-    int lenght = collection.Lenght;
+    int lenght = collection.Length;
     int index = 0;
     while(index < lenght)
     {
@@ -21,7 +21,31 @@ void PrintArray(int[] col)
     }
 }
 
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while(index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
 int[] array = new int [10]; //new int[10] создай новый массив в которой будет 10 эллементов 
 
 FillArray(array);
+array[4] = 4;
+array[6] = 4;
+
 PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 4);
+Console.WriteLine(pos);
