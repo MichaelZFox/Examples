cbe09dd5245811f777958b95e27162fbd97d12ff
«Mike <fox9403@gmail.com>
2022-08-30 15:17:21 +0300

    add ex007_array

---
+++ ex007_array/Program.cs
@@ -0,0 +1,28 @@
+﻿void Fillarrat(int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+}
+while(index < lenght)
+{
+    cillection[index] = new Random().Next(1, 10);
+    index++;
+}
+
+void PrintArray(int[] collection);
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count)
+    {
+        Console.Writeline(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int [10];
+
+FillArray(array);
+PrintArray(array);
